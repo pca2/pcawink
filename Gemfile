d31823b23c028a75db839633e22263b5f8dc1ae0
pca2 <atwaterc@gmail.com>
2016-06-11 11:15:53 -0400

    rewrite get_creds.rb

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 gem 'wink', path: "~/Git/pcawink/wink"
+gem 'rest-client'
+gem 'json'
 gem 'pry'
--- get_creds.rb
+++ get_creds.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 #get credentials
-require 'rest_client'
+require 'rest-client'
+require 'json'
 require_relative 'CREDENTIALS.rb'
-values   = "{\n    \"client_id\": \"#{CLIENT_ID}\",\n    \"client_secret\": \"#{CLIENT_SECRET}\",\n    \"username\": \"#{USERNAME}\",\n    \"password\": \"#{PASSWORD}\",\n    \"grant_type\": \"password\"\n}"
-headers  = {:content_type => "application/json"}
-response = RestClient.post "https://winkapi.quirky.com/oauth2/token", values, headers
-puts response
+@values = {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, username: USERNAME, password: PASSWORD, grant_type: "password"}
+json_values = @values.to_json
+headers  = {content_type: "application/json"}
+json_response = RestClient.post "https://winkapi.quirky.com/oauth2/token",json_values, headers
+@response = JSON.parse(json_response)
+puts "access_token: " + @response["access_token"]
+puts "refresh_token: " + @response["refresh_token"]
